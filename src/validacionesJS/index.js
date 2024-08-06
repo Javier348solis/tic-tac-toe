6413b64e2a26a0d067758af2f97b0c33922e558c
Javier348solis <javiersolisretana@gmail.com>
2024-08-06 11:06:01 -0600

    commit

--- src/validacionesJS/index.js
+++ src/validacionesJS/index.js
@@ -3,20 +3,36 @@ const botonI = document.getElementById("botoncito");
 const celdasTabla = Array.from(document.querySelectorAll(".cuadritos"));
 const opciones = document.getElementById("mostrarOpciones")
 
+const primerESP = document.getElementById("Espacio1")
+const segundoESP = document.getElementById("Espacio1")
+const tercerESP = document.getElementById("Espacio1")
+const cuartoESP = document.getElementById("Espacio1")
+const quintoESP = document.getElementById("Espacio1")
+const sexoESP = document.getElementById("Espacio1")
+const septimoESP = document.getElementById("Espacio1")
+const octavoESP = document.getElementById("Espacio1")
+const novenoESP = document.getElementById("Espacio1")
+
+
+const arregloCasillas= [primerESP,segundoESP,tercerESP,cuartoESP,quintoESP,sexoESP,septimoESP,octavoESP,novenoESP]
 
 let primerJUga = true;
 let seguJugador = false;
 
 const simbolosJugador = (e) =>{
-    const tablaJuga = e.target.innerHTML
-    if (!tablaJuga) {
-        e.target.innerHTML = primerJUga ? "❌" : "⭕";
-        primerJUga = !primerJUga;
-        posiblidadesJuego();
-        if (!seguJugador) funciComputadora();
-    }
-    opciones.classList.toggle("js", e.target.innerHTML === "❌");
-    opciones.classList.toggle("js2", e.target.innerHTML === "⭕");
+    arregloCasillas.forEach((casilla)=>casilla.addEventListener("click",()=>{
+        casilla.innerHTML="x"
+    }))
+
+    // const tablaJuga = e.target.innerHTML
+    // if (!tablaJuga) {
+    //     e.target.innerHTML = primerJUga ? "❌" : "⭕";
+    //     primerJUga = !primerJUga;
+    //     posiblidadesJuego();
+    //     if (!seguJugador) funciComputadora();
+    // }
+    // opciones.classList.toggle("js", e.target.innerHTML === "❌");
+    // opciones.classList.toggle("js2", e.target.innerHTML === "⭕");
 };
 const funciComputadora = () => {
     const casillasVacias = celdasTabla.filter((celda) => !celda.innerHTML);
